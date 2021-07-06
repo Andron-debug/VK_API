c166bb67ad98789241a3a1102f9346682b608bd7
Andron-debug <b.a.a.2002@yandex.ru>
2021-07-06 11:34:40 +0300

    Добавить вывод друзей в listbox

---
+++ VK_API/Firend_info.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace VK_API
+{
+    partial class Firend_info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Friend = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 16);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(366, 416);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Friend
+            // 
+            this.Friend.FormattingEnabled = true;
+            this.Friend.ItemHeight = 16;
+            this.Friend.Location = new System.Drawing.Point(563, 12);
+            this.Friend.Name = "Friend";
+            this.Friend.Size = new System.Drawing.Size(225, 420);
+            this.Friend.TabIndex = 1;
+            this.Friend.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Firend_info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Friend);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Firend_info";
+            this.Text = "Firend_info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Friend;
+    }
+}
---
+++ VK_API/Firend_info.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace VK_API
+{
+    public partial class Firend_info : Form
+    {
+        string token;
+        public Firend_info(string tok)
+        {
+            InitializeComponent();
+            token = tok;
+            var api_user = new VkApi();
+            // обработать исключения!
+            api_user.Authorize(new ApiAuthParams
+            {
+                AccessToken = token
+            });
+            // получить список друзей (для пользователя)
+            var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                Fields = VkNet.Enums.Filters.ProfileFields.All
+            });
+            foreach (User user in getFriends)
+                Friend.Items.Add(Encoding.UTF8.GetString(Encoding.Default.GetBytes(user.FirstName)) + " " + Encoding.UTF8.GetString(Encoding.Default.GetBytes(user.LastName)));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frend_info_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
